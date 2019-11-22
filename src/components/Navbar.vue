a602396006be4ce1f726b385afc47b8042251bcf
grecendiz24 <grecendiz24@gmail.com>
2019-11-21 20:31:35 -0600

    -save

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,7 @@
       <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Servi-tech</v-toolbar-title>
-
       <div class="flex-grow-1"></div>
-
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
